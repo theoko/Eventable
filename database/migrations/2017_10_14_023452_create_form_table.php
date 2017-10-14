ae3fdef34bebdbe32df9e7814ce977d9fc7f6c28
keiferchiang <keiferchiang@gmail.com>
2017-10-14 00:32:14 -0400

    initial commit

---
+++ database/migrations/2017_10_14_023452_create_form_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFormTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('form', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name'); // The name of the event
+            $table->string('orgname'); // The name of the organization
+            $table->string('address', 255); // The address of the event
+            $table->text('description'); // The description of the event
+            $table->dateTime('date'); // The event date
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('form');
+    }
+}
